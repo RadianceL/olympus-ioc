8329bb30f8a2c76492af213db1e8b3ae9dcd776e
Eddie Lee <931305033@qq.com>
2018-12-27 12:59:12 +0800

    init: init full repository space

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * @author eddie
+ * @createTime 2018-12-27
+ * @description 主程序入口
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
